45c9b8d4b01dfce92bfe8eda5164f916cf97aee5
Zenit95 <javiermartineztejero@gmail.com>
2017-02-28 09:27:15 +0100

    Añadidos Especie, SerVivo y Planeta

--- models.py
+++ models.py
@@ -2,9 +2,22 @@
 
 from openerp import fields, models
 
-class Especies(models.Model):
-    _name = 'examen.especies'
+class Especie(models.Model):
+    _name = 'examen.especie'
     
     name = fields.Char(string="Especie", required=True)
     
+class SerVivo(models.Model):
+    _name = 'examen.servivo'
+    
+    especie_id = fields.Many2one('examen.especies',
+            string="Especie")
+    
+class Planeta(models.Model):
+    _name = 'examen.planeta'
+    
+    name = fields.Char(string="Planeta", required=True)
+    distancia = fields.Integer();
+    existe = fields.Boolean(default=False)
+    fechaDestruccion = fields.Date()
     
